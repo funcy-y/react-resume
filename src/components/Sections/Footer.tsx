04bb87dc41a52db77f98ab8bbc9652feb59b8fbb
yangfan <152611123964@163.com>
2024-03-30 15:00:43 +0800

    注释

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -2,7 +2,7 @@ import {BoltIcon, ChevronUpIcon} from '@heroicons/react/24/solid';
 import {FC, memo} from 'react';
 
 import {SectionId} from '../../data/data';
-import Socials from '../Socials';
+// import Socials from '../Socials';
 
 const currentYear = new Date().getFullYear();
 
